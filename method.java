efe1183617a80ecc5a86448ccc647fcf88c31ae8
Ashrafkhan07 <68174745+Ashrafkhan07@users.noreply.github.com>
2021-10-10 18:10:19 +0530

    Update method.java

--- method.java
+++ method.java
@@ -1,9 +1,7 @@
 class Main {
 
-  // create a method
   public int addNumbers(int a, int b) {
     int sum = a + b;
-    // return value
     return sum;
   }
 
@@ -12,9 +10,7 @@ class Main {
     int num1 = 25;
     int num2 = 15;
 
-    // create an object of Main
     Main obj = new Main();
-    // calling method
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
